1ec5988f7fa140eaca3303fefd689e69c7f354e1
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-09-27 21:06:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,23 +7,25 @@ import shutil
 
 class Archive(AddOn):
     def main(self):
-        os.makedirs(os.path.dirname('./ia/'), exist_ok=True)
+        os.makedirs(os.path.dirname('./out/'), exist_ok=True)
         pname = self.data.get('pname') 
         iname = self.data.get('iname')
         iname = iname.replace(' ', '-')
         p = self.client.projects.get(id=None, title=pname)
+        ia_user=os.environ["IA_USER"]
+        ia_pass=os.environ["IA_PASS"]
+        os.system('ia configure -u {ia_user} -p {ia_pass}')
         
         for i in p.document_ids:
             d = self.client.documents.get(i)
             p = d.pdf
             t = d.title + ".pdf"
-            save_path='./ia'
+            save_path='./out'
             full_path = os.path.join(save_path, t)
             with open(full_path, 'wb') as f:
                 f.write(d.pdf)
             r = upload(iname, files = full_path)
-        shutil.rmtree('./ia/', ignore_errors=False, onerror=None)
-        
+        shutil.rmtree('./out/', ignore_errors=False, onerror=None)
 
 if __name__ == "__main__":
     Archive().main()
