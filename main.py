c691deec8175fbc53d9f9f1c3d139f0a0af329bd
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-09-29 09:16:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ class Archive(AddOn):
         p = self.client.projects.get(id=None, title=pname)
         ia_user=os.environ["IA_USER"]
         ia_pass=os.environ["IA_PASS"]
-        cmd = 'ia configure --username ' + ia_user + ' ' + '--password ' + ia_pass
-        os.system(cmd)
+        # cmd = 'ia configure --username ' + ia_user + ' ' + '--password ' + ia_pass
+        # os.system(cmd)
         
         for i in p.document_ids:
             d = self.client.documents.get(i)
